401bd5640b13461584a3261a597902ac0e6927d3
ba_c <swunhtetoo@gmail.com>
2023-06-21 09:38:58 +0630

    adding ref by chapter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Please type the name of file resource in URL to download it.
+        This is under development.
+        Please add /fileResources/[filename] in the URL to download it.
         Thank you.
       </div>
     </main>
